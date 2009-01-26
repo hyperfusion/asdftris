c8703eca0356e478f5df9d67606e0e99f5566e60
Jonathan Ho <hojonathanho@gmail.com>
2009-01-25 16:32:25 -0800

    falling (timing), rotation, collision detection

--- asdftris.py
+++ asdftris.py
@@ -18,6 +18,7 @@ BLOCK_COLORS = (
     (160, 32, 240), # T
     (255, 0, 0)     # Z
 )
+FALL_DELAY = 1000
 
 BLOCK_DEF = (
     (((0, 1), (1, 1), (2, 1), (3, 1)), 4), # I
@@ -30,21 +31,29 @@ BLOCK_DEF = (
 )
 
 SQUARES = []
+FIELD = [[None] * SCREEN_SIZE[1] for i in range(SCREEN_SIZE[0])]
 
 class Block:
-    def draw(self, screen):
-        for i in range(4):
-            for j in range(4):
-                if self.c[i][j]:
-                    screen.blit(SQUARES[self.type], ((self.pos[0] + i) * SQ_SIZE, (self.pos[1] + j) * SQ_SIZE))
-
     def create(self, type):
         self.type = type
         self.c = [[False] * 4 for i in range(4)]
         for i in BLOCK_DEF[type][0]: self.c[i[0]][i[1]] = True
         self.size = BLOCK_DEF[type][1]
         self.color = BLOCK_COLORS[type]
-        self.pos = [0, 0]
+        self.x, self.y = 0, 0
+
+    def collides(self, dx, dy):
+        for i in range(4):
+            for j in range(4):
+                a, b = self.x + dx + i, self.y + dy + j
+                if self.c[i][j] and (a < 0 or b < 0 or a >= SCREEN_SIZE[0] or b >= SCREEN_SIZE[1] or FIELD[a][b]):
+                    return True
+        return False
+
+    def move(self, dx, dy):
+        if self.collides(dx, dy): return
+        self.x += dx
+        self.y += dy
 
     def rotate(self, dir):
         d = [[False] * 4 for i in range(4)]
@@ -54,14 +63,18 @@ class Block:
                     d[self.size - j - 1][i] = self.c[i][j]
                 else:
                     d[j][self.size - i - 1] = self.c[i][j]
+        for i in range(4):
+            for j in range(4):
+                a, b = self.x + i, self.y + j
+                if d[i][j] and (a < 0 or b < 0 or a >= SCREEN_SIZE[0] or b >= SCREEN_SIZE[1] or FIELD[a][b]):
+                    return
         self.c = d
 
-class Grid:
-    def __init__(self):
-        self.grid = [[None] * SCREEN_SIZE[0] for i in SCREEN_SIZE[1]]
-
-    def add(self, block):
-        pass
+    def draw(self, screen):
+        for i in range(4):
+            for j in range(4):
+                if self.c[i][j]:
+                    screen.blit(SQUARES[self.type], ((self.x + i) * SQ_SIZE, (self.y + j) * SQ_SIZE))
 
 def main():
     pyg.init()
@@ -78,24 +91,33 @@ def main():
         pyg.draw.line(i, SQ_BORDER2, (0, SQ_SIZE-1), (SQ_SIZE-1, SQ_SIZE-1))
         pyg.draw.line(i, SQ_BORDER2, (SQ_SIZE-1, 0), (SQ_SIZE-1, SQ_SIZE-1))
         SQUARES.append(i)
-
+    
     b = Block()
     b.create(0)
 
+    last_fall = 0
     while True:
         for event in pyg.event.get():
             if event.type == QUIT: return
             elif event.type == KEYDOWN:
-                if event.key == K_ESCAPE: return
-                elif event.key == K_a: b.rotate(True)
-                elif event.key == K_b: b.rotate(False)
-                elif event.key == K_0: b.create(0)
-                elif event.key == K_1: b.create(1)
-                elif event.key == K_2: b.create(2)
-                elif event.key == K_3: b.create(3)
-                elif event.key == K_4: b.create(4)
-                elif event.key == K_5: b.create(5)
-                elif event.key == K_6: b.create(6)
+                k = event.key
+                if k == K_ESCAPE: return
+                elif k == K_k: b.rotate(True)
+                elif k == K_i: b.rotate(False)
+                elif k == K_h: b.move(-1, 0)
+                elif k == K_l: b.move(1, 0)
+                elif k == K_0: b.create(0)
+                elif k == K_1: b.create(1)
+                elif k == K_2: b.create(2)
+                elif k == K_3: b.create(3)
+                elif k == K_4: b.create(4)
+                elif k == K_5: b.create(5)
+                elif k == K_6: b.create(6)
+
+        time = pyg.time.get_ticks()
+        if time - last_fall >= FALL_DELAY:
+            b.move(0, 1)
+            last_fall = time
 
         screen.fill(SCREEN_BG_COLOR)
         b.draw(screen)
